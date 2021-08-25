2d25b39f8a35fbe6cf48534b3ef465e47075b119
Katie Klimkowski <Katie.Klimkowski@sckans.edu>
2021-08-25 16:33:15 -0500

    edit

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
       ref={childRef}
       children={() => (
         <Switch>
-          <AppRoute exact path="/portfolio" component={Home} layout={LayoutDefault} />
+          <AppRoute exact path="/porfolio" component={Home} layout={LayoutDefault} />
         </Switch>
       )} />
   );
